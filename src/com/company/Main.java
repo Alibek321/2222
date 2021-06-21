58e1aa7da4c444e999f510f37e02132b715ae4c0
Alibek2 <ali.nurbekov07@gmail.com>
2021-06-21 18:06:33 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String name = " alibek  ";
+        int age = 22;
+        int temp = 15;
+
+        if ((age >= 15 && age <= 40) && (temp >= -25 && temp <= 30)) ;
+        System.out.println(name + " can go");
+
+    }
+}
+
